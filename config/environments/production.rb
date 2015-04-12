f924e16a893160aca75e0a46f841c756dc2f189c
Stan Peev <stan.peev@gmail.com>
2015-04-12 16:51:39 +0100

    Configured app for Production emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,10 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # http://stan-bloccit.herokuapp.com
+  config.action_mailer.default_url_options = { host: 'stan-bloccit.herokuapp.com'}
+
+  #Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
+  config.force_ssl = true
 end
