0096d1e5b0f053fbdaea6ff3f2b6a2538769811e
Stan Peev <stan.peev@gmail.com>
2015-04-13 16:32:56 +0100

    Validating posts checkpoint completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,4 +10,9 @@ class Post < ActiveRecord::Base
 
   scope :ordered_by_title, -> { order('posts.title ASC') }
   scope :ordered_by_reverse_created_at, -> { order('posts.created_at DESC') }
+
+      validates :title, length: { minimum: 5 }, presence: true
+      validates :body, length: { minimum: 20 }, presence: true
+      validates :topic, presence: true
+      validates :user, presence: true
 end
