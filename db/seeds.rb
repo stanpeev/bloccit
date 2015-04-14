9cb57c1f4e850f4085b4fdd2b8b4af1752b2883b
Stan Peev <stan.peev@gmail.com>
2015-04-14 21:43:04 +0100

    Pagination checkpoint complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,7 +32,7 @@ topics = Topic.all
  # The `save` method then saves this User to the database.
 
 #Create Posts
-50.times do
+160.times do
   Post.create!(
     user: users.sample,
     topic: topics.sample,
