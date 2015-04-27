6c314bbc59738bcc67bfaa62ddd355b3e8970542
Stan Peev <stan.peev@gmail.com>
2015-04-27 22:08:44 +0100

    Private topics checkpoint complete

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,6 +1,9 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
 
+    scope :visible_to, -> (user) { user ? all : where(public: true) }
+
+
       validates :name, length: { minimum: 5 }, presence: true
 
    self.per_page = 50
