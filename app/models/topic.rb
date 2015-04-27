14cfbf650299119ac99f64a5236c01afe2690e7e
Stan Peev <stan.peev@gmail.com>
2015-04-27 23:29:39 +0100

    Private topics assignment complete

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,7 +1,12 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
 
-    scope :visible_to, -> (user) { user ? all : where(public: true) }
+    scope :publicly_viewable, -> { where(public: true) }
+    scope :privately_viewable, -> { where(public: false) }
+
+    scope :visible_to, -> (user) { user ? all : publicly_viewable }
+
+   
 
 
       validates :name, length: { minimum: 5 }, presence: true
