df191457c7cfd3eafdc17f6e0345b550af2bec31
Stan Peev <stan.peev@gmail.com>
2015-04-28 19:27:59 +0100

    Another interlude checkpoint complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,5 +25,9 @@ class User < ActiveRecord::Base
  def favorited(post)
     favorites.where(post_id: post.id).first
   end
+
+  def voted(post)
+    votes.where(post_id: post.id).first
+  end
 end
 
