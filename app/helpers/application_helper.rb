306122259d7ac3ca1c630e3cfe97c17bba5ad7fd
Stan Peev <stan.peev@gmail.com>
2015-04-28 20:08:24 +0100

    Another interlude assignment complete

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,20 @@ module ApplicationHelper
       content_tag :div, capture(&block), class: 'form-group'
     end
   end
+
+  def up_vote_link_classes(post)
+      if current_user.voted(post) && current_user.voted(post).up_vote?
+       'voted'
+     else
+      ''
+     end
+  end
+
+  def down_vote_link_classes(post)
+    if current_user.voted(post) && current_user.voted(post).down_vote?
+       'voted'
+     else
+      ''
+     end
+  end
 end
