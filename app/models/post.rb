1f30ff6592f611290d12e305e72a0192475df0a3
Stan Peev <stan.peev@gmail.com>
2015-04-29 16:16:44 +0100

    Public profiles checkpoint complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -27,6 +27,7 @@ class Post < ActiveRecord::Base
   end
 
   default_scope { order('rank DESC') }
+    scope :visible_to, -> (user) { user ? all : joins(:topic).where('topics.public' => true) }
 
   scope :ordered_by_title, -> { order('posts.title ASC') }
   scope :ordered_by_reverse_created_at, -> { order('posts.created_at DESC') }
